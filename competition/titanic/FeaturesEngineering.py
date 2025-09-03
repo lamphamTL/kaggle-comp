82e2e5974dcbe8eb52b60e7fba223e3abb0cb630
lamp <lam.pham@thetrainline.com>
2025-09-03 23:43:31 +0200

    XGBoost model - The prediction is worse :(

---
+++ competition/titanic/FeaturesEngineering.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+import warnings
+
+warnings.filterwarnings("error", category=FutureWarning)
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, MultiLabelBinarizer
+
+
+# Since MultiLabelBinarizer only take (y) during fit and fit_transform, we create this wrapper
+# to align it with other transformers.
+# It's the same idea as OneHotEncoder -> y is passed to fit() but not used
+class MultiLabelBinarizerTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, **kwargs):
+        self.classes_ = None
+        self.mlb = MultiLabelBinarizer(**kwargs)
+
+    def fit(self, X, y=None):  # accepts (X, y)
+        self.mlb.fit(X)
+        # This is important because scikit-learn verify estimator's fittedness based on existence of
+        # attribute ending with "_" and not starting with "__"
+        # Here classes_ is just overwritten from MultiLabelBinarizer
+        self.classes_ = self.mlb.classes_
+        return self
+
+    def transform(self, X):
+        return self.mlb.transform(X)
+
+    def fit_transform(self, X, y=None):
+        return self.mlb.fit_transform(X)
+
+    def get_feature_names_out(self, input_features=None):
+        return self.mlb.classes_
+
+
+def engineer_features():
+    # Engineer "Cabin" -> "HasCabin"
+    has_cabin_transformer = FunctionTransformer(lambda x: x.notnull().astype(int), validate=False)
+    decks = Pipeline(
+        [
+            ("cabin_to_deck", FunctionTransformer(lambda y: y.iloc[:, 0].apply(
+                lambda c: ['U'] if pd.isna(c) else list({part[0] for part in c.split()})).tolist(), validate=False)),
+            ("mlb", MultiLabelBinarizerTransformer())
+        ]
+    )
+    cat_variables_transf = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+    return ColumnTransformer(
+        [
+            ("has_cabin", has_cabin_transformer, ["Cabin"]),
+            ("decks", decks, ["Cabin"]),
+            ("dropper", "drop", ["Name", "Ticket", "Cabin"]),
+            ("cat_variables", cat_variables_transf, ["Sex", "Embarked"])
+        ]
+    )
+
+
+df = pd.read_csv("../data/train.csv", delimiter=",")
+
+features = [x for x in df.columns if x != "Survived"]
+x, y = df[features], df["Survived"].T
+print(x.shape)
+print(y.shape)
